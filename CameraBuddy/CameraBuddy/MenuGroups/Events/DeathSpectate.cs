b378b69867cb73a88c23c42a466ef09486f02ef7
Lachlan van der Velden <lvelden1@gmail.com>
2016-01-27 22:06:11 +1100

    Fix Compile Error

---
+++ CameraBuddy/CameraBuddy/MenuGroups/Events/DeathSpectate.cs
@@ -0,0 +1,40 @@
+﻿using CameraBuddy.Camera;
+using CameraBuddy.Spectate;
+using EloBuddy;
+using EloBuddy.SDK.Menu;
+
+namespace CameraBuddy.MenuGroups.Events
+{
+    public class DeathSpectate : MenuGroup 
+    {
+        public bool IsPlayerDead { get { return Player.Instance.IsDead; } }
+        public Spectator Spectator { get; set; }
+        public DeathSpectate()
+        {
+            Spectator = new Spectator();
+            EloBuddy.Game.OnUpdate += Game_OnUpdate;
+        }
+
+        private void Game_OnUpdate(System.EventArgs args)
+        {
+            if(Enabled)
+                Spectator.Start();
+            else
+                Spectator.Stop();
+
+        }
+
+        public override void AddToMenu(Menu menuBase)
+        {
+            var menu = menuBase.Parent.AddSubMenu("     Death Spectate");
+            base.AddToMenu(menu);
+            
+
+        }
+
+        public override string GetUniqueId()
+        {
+            return "DeathSpectate";
+        }
+    }
+}
